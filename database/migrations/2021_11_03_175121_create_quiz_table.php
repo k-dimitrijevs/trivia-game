c1b0f9100aeaf34c285efa0197b8fd1dbff727ea
k-dimitrijevs <kristians.dimitrijevs@gmail.com>
2021-11-03 20:00:55 +0200

    initial commit

---
+++ database/migrations/2021_11_03_175121_create_quiz_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuizTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quiz', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id');
+            $table->string('question')->nullable();
+            $table->integer('answer')->nullable();
+            $table->integer('correct_answers_count')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('quiz');
+    }
+}
